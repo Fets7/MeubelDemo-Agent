8c39aebfa1b4a81515b3782c843beb28693824f0
Fets7 <143446190+Fets7@users.noreply.github.com>
2025-10-28 17:39:50 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-"use client";
+import App from "./App";
 
-import { Chat } from "@openai/chatkit/react";
-import type { ChatKitOptions } from "@openai/chatkit";
-
-const options: ChatKitOptions = {
-  api: {
-    url: "/api/chat", // Zorgt dat het type klopt ✅
-  },
-  theme: {
-    colorScheme: "light",
-    radius: "pill",
-    density: "normal",
-    color: {
-      accent: {
-        primary: "#2900f5",
-      },
-    },
-  },
-  startScreen: {
-    greeting: "Waar kan ik je vandaag mee helpen?",
-    prompts: [],
-  },
-};
-
-export default function Page() {
-  return <Chat options={options} />;
+export default function Home() {
+  return <App />;
 }
